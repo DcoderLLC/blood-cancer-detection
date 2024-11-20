fe5d241fe4bc996cd1825efbfc55603199ad8bed
DcoderLLC <dcoderllc@gmail.com>
2024-11-20 18:46:06 +0530

    Page routed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,24 @@
 import "./App.css";
 import { Box } from "@mui/material";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./Components/NavBar/Navbar";
+import Dashboard from "./Pages/Dashboard";
+import Reports from "./Pages/Reports";
+import About from "./Pages/About";
+import Contact from "./Pages/Contact";
 
 function App() {
   return (
     <Box>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" exact element={<Dashboard />} />
+          <Route path="/" exact element={<Reports />} />
+          <Route path="/" exact element={<About />} />
+          <Route path="/" exact element={<Contact />} />
+        </Routes>
+      </Router>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
     </Box>
   );
