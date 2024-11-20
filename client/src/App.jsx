da7f44cd00639869a425efde46eb7e3e413335f9
DcoderLLC <dcoderllc@gmail.com>
2024-11-20 16:15:34 +0530

    project init with dependencies install

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import { Box } from "@mui/material";
+
+function App() {
+  return (
+    <Box>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </Box>
+  );
+}
+
+export default App;
